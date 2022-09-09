086c35f9553a0b0d6fabc562155482ef564bf229
Iryna Movchan <movchaniryna@ukr.net>
2022-09-09 12:32:54 +0300

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+stopBtn.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtn() {
+  body.style.backgroundColor = getRandomHexColor();
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtn() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
